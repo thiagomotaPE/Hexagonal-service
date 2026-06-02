8f27525383e2b5bc9b354fcfa48829d81a43f5d2
Mota <thiagomotape@hotmai.com.br>
2026-06-02 18:19:12 -0300

    feat: criando client para validar endereço

---
+++ hexagonal/src/main/java/com/mota/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -0,0 +1,15 @@
+package com.mota.hexagonal.adapters.out.client;
+
+import com.mota.hexagonal.adapters.out.client.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "FindAddressByZipCodeClient",
+        url = "${mota.client.address.url}"
+)
+public interface FindAddressByZipCodeClient {
+    @GetMapping("/zipCode")
+    AddressResponse find(@PathVariable("zipCode") String zipCode);
+}
