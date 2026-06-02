0722a5a1672d0fa1a2bdaf35730ba59a4b7193f3
Mota <thiagomotape@hotmai.com.br>
2026-06-02 18:51:04 -0300

    feat: criando repositoy para persistir dados do cliente

---
+++ hexagonal/src/main/java/com/mota/hexagonal/adapters/out/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.mota.hexagonal.adapters.out.repository;
+
+import com.mota.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+}
---
+++ hexagonal/src/main/java/com/mota/hexagonal/adapters/out/repository/entity/CustomerEntity.java
@@ -0,0 +1,16 @@
+package com.mota.hexagonal.adapters.out.repository.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "customers")
+public class CustomerEntity {
+    @Id
+    private String id;
+    private String name;
+    private AddressEntity address;
+    private String cpf;
+    private Boolean isValidCpf;
+}
